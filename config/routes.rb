e55a3594bba5fce91879e2135981f64c35dd0ff2
slee713 <steven.lee1393@gmail.com>
2020-08-06 22:56:00 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :genres, except: [:destroy]
+  resources :artists, except: [:destroy]
+  resources :songs, except: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.destroy_all
+Song.destroy_all
+Genre.destroy_all
+
+joyner = Artist.create(name:"Joyner Lucas", bio: "I am a rapper")
+john = Artist.create(name:"John Mayer", bio: "I am the best guitarist")
+ed = Artist.create(name:"Ed Sheeran", bio: "I am very good at singing")
+drake = Artist.create(name:"Drake", bio: "I am overrated")
+
+rap = Genre.create(name:"Rap")
+pop = Genre.create(name:"Pop")
+blues = Genre.create(name: "Blues")
+
+Song.create(name:"I Am Not Racist", artist_id: joyner.id, genre_id: rap.id)
+Song.create(name:"Isis", artist_id: joyner.id, genre_id: rap.id)
+Song.create(name:"Slow Dancing in a Burning Room", artist_id: john.id, genre_id: blues.id)
+Song.create(name:"Perfect", artist_id: ed.id, genre_id: pop.id)
+Song.create(name:"Pop Style", artist_id: drake.id, genre_id: rap.id)
